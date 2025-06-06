6cc0fd7d531ddf3600d307d7ab1dcac8ee61d8f8
Navanish Mehta <Navanish@097>
2025-06-06 13:28:32 +0530

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  basePath: '/Mystichat-App',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
