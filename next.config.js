dc476e655eecb354e097685f7b796f986ac9096b
Navanish-Mehta <dadanmehta097@gmail.com>
2025-06-06 14:19:23 +0530

    Fix GitHub Pages deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
   output: 'export',
   basePath: '/Mystichat-App',
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
-  distDir: 'out',
   assetPrefix: '/Mystichat-App/',
+  reactStrictMode: true,
+  swcMinify: true,
 }
 
 module.exports = nextConfig
